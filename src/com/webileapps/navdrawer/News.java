b57b1af15665e169ec49bd68c07988082dfff8ab
syafik <syafiklijtk04@gmail.com>
2013-09-23 12:47:16 +0700

    implemented news api

--- src/com/webileapps/navdrawer/News.java
+++ src/com/webileapps/navdrawer/News.java
@@ -2,6 +2,14 @@ package com.webileapps.navdrawer;
 
 
 
+import java.io.BufferedInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
 import java.util.ArrayList;
 import java.util.List;
 import org.json.JSONArray;
@@ -12,12 +20,21 @@ import com.persipura.bean.NewsBean;
 import com.persipura.utils.*;
 
 
+import android.app.Service;
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.StrictMode;
+import android.text.Html;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
@@ -47,10 +64,11 @@ public class News extends SherlockFragment {
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
-//		new fetchLocationFromServer().execute("");
+		new fetchLocationFromServer().execute("");
 		View rootView = inflater.inflate(R.layout.news, container,
 				false);
-		
+		 mInflater = getLayoutInflater(savedInstanceState);
+
 //		mParentLayout = (RelativeLayout) mInflater.inflate(
 //				R.layout.news, null);
 		lifePageCellContainerLayout = (LinearLayout) rootView
@@ -166,32 +184,52 @@ protected void onProgressUpdate(Void... values) {
 
 @Override
 protected void onPostExecute(String result) {
-	Log.d("-----------------", result);
-//	if (result != null && !result.equals("")) {
-		
+	Log.d("-----------------", "weyyy :" + result.getClass().getName());
+	
 		try {
-			JSONObject resObject = new JSONObject(result);
-			Log.d("-----------------", "asdfghjkl");
+//			JSONObject resObject = new JSONObject(result);
+            JSONArray jsonArray = new JSONArray(result);
+            
+            listThisWeekBean = new ArrayList<NewsBean>();
+			for (int i = 0; i < jsonArray.length(); i++) {
+                JSONObject resObject = jsonArray.getJSONObject(i);
+                NewsBean thisWeekBean = new NewsBean();
+                thisWeekBean.setNid(resObject.getString("nid"));
+				thisWeekBean.settitle(resObject.getString("title"));
+				thisWeekBean.setteaser(resObject.getString("teaser"));
+				thisWeekBean.setimg_uri(resObject.getString("img_uri"));
+				
+				
+				// String sucess1 =
+				// resObject1.getString("close_at");
+				
+				listThisWeekBean.add(thisWeekBean);
+            }
+			if (listThisWeekBean != null
+					&& listThisWeekBean.size() > 0) {
+				// createSeqUnderLine(lifePageCellContainerLayout);
+				createSelectLocationListView(listThisWeekBean);
+			}
 //				JSONArray jsonArray = resObject
 //						.getJSONArray("restaurants");
-				listThisWeekBean = new ArrayList<NewsBean>();
-				for (int i = 0; i < resObject.length(); i++) {
-					NewsBean thisWeekBean = new NewsBean();
-					thisWeekBean.setNid(resObject.getString("nid"));
-					thisWeekBean.settitle(resObject.getString("title"));
-					thisWeekBean.setteaser(resObject.getString("teaser"));
-					
-					
-					// String sucess1 =
-					// resObject1.getString("close_at");
-					
-					listThisWeekBean.add(thisWeekBean);
-				}
-				if (listThisWeekBean != null
-						&& listThisWeekBean.size() > 0) {
-					// createSeqUnderLine(lifePageCellContainerLayout);
-					createSelectLocationListView(listThisWeekBean);
-				}
+//				listThisWeekBean = new ArrayList<NewsBean>();
+//				for (int i = 0; i < resObject.length(); i++) {
+//					NewsBean thisWeekBean = new NewsBean();
+//					thisWeekBean.setNid(resObject.getString("nid"));
+//					thisWeekBean.settitle(resObject.getString("title"));
+//					thisWeekBean.setteaser(resObject.getString("teaser"));
+//					
+//					
+//					// String sucess1 =
+//					// resObject1.getString("close_at");
+//					
+//					listThisWeekBean.add(thisWeekBean);
+//				}
+//				if (listThisWeekBean != null
+//						&& listThisWeekBean.size() > 0) {
+//					// createSeqUnderLine(lifePageCellContainerLayout);
+//					createSelectLocationListView(listThisWeekBean);
+//				}
 			
 		} catch (Exception e) {
 			e.printStackTrace();
@@ -199,35 +237,81 @@ protected void onPostExecute(String result) {
 //	}
 }
 
+@SuppressWarnings("deprecation")
 private void createSelectLocationListView(
 		List<NewsBean> listThisWeekBean) {
 	for (int i = 0; i < listThisWeekBean.size(); i++) {
 		NewsBean thisWeekBean = listThisWeekBean.get(i);
 		
-			RelativeLayout cellViewMainLayout = (RelativeLayout) mInflater
-					.inflate(R.layout.news_list, null);
-			TextView nameTextView = (TextView) cellViewMainLayout
+
+			
+			View cellViewMainLayout = mInflater
+				.inflate(R.layout.news_list, null);
+			TextView titleNews = (TextView) cellViewMainLayout
 					.findViewById(R.id.findzoes_list_text_name);
-			TextView addressTextView = (TextView) cellViewMainLayout
+			TextView descNews = (TextView) cellViewMainLayout
 					.findViewById(R.id.findzoes_list_text_address);
 			TextView cellnumTextView = (TextView) cellViewMainLayout
 					.findViewById(R.id.findzoes_list_text_cellnum);
+			ImageView imgNews = (ImageView) cellViewMainLayout
+					.findViewById(R.id.imageView1);
 			
-			nameTextView.setText("");
-			addressTextView.setText("");
+			titleNews.setText("");
+			descNews.setText("");
 			cellnumTextView.setText("");
 			
 
-			nameTextView.setText(thisWeekBean.gettitle());
-			addressTextView.setText(thisWeekBean.getteaser());
+			titleNews.setText(thisWeekBean.gettitle());
+			descNews.setText(Html.fromHtml(thisWeekBean.getteaser()));
+	        BitmapFactory.Options bmOptions;
 
-		
+			  bmOptions = new BitmapFactory.Options();
+		        bmOptions.inSampleSize = 1;
+			Bitmap bm = loadBitmap(thisWeekBean.getimg_uri(), bmOptions);
+			
+			imgNews.setImageBitmap(bm);
+					
 			lifePageCellContainerLayout.addView(cellViewMainLayout);
 			
 		
 	}
 }
 
+
+
+}
+public static Bitmap loadBitmap(String imgurl, BitmapFactory.Options options) {
+	try {
+	    if (android.os.Build.VERSION.SDK_INT > 9) {
+	        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+	        StrictMode.setThreadPolicy(policy);
+	      }
+	    
+        URL url = new URL(imgurl);
+		InputStream in = url.openConnection().getInputStream(); 
+        BufferedInputStream bis = new BufferedInputStream(in,1024*8);
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+
+        int len=0;
+        byte[] buffer = new byte[1024];
+        while((len = bis.read(buffer)) != -1){
+            out.write(buffer, 0, len);
+        }
+        out.close();
+        bis.close();
+
+        byte[] data = out.toByteArray();
+        Bitmap bitmap = BitmapFactory.decodeByteArray(data, 0, data.length);
+        return bitmap;
+//        URL url = new URL(imgurl);
+//        Bitmap bitmap = BitmapFactory.decodeStream(url.openConnection().getInputStream());
+//        return bitmap;
+    }
+    catch (IOException e) {
+        e.printStackTrace();
+        return null;
+
+    }
 }
 	
 }
